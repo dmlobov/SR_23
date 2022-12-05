f32cdbbe300917ca2f522b4b3aa134a0a6f36901
D L <d.m.lobov@yandex.ru>
2022-12-05 15:45:13 +0300

    Добавьте файлы проекта.

---
+++ App2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace App2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            Sum(n);
+            Fact(n);
+            Console.ReadKey();
+
+        }
+
+        static void Sum(int n)
+        {
+            int s = 0;
+            for (int i = 0; i < n; i++)
+            {
+                s += i;
+                Thread.Sleep(100);
+            }
+            Console.WriteLine(s);
+        }
+        static void Fact(int n)
+        {
+            int s = 1;
+            for (int i = 1; i < n; i++)
+            {
+                s *= i;
+                Thread.Sleep(100);
+            }
+            Console.WriteLine(s);
+        }
+    }
+}
